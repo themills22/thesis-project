b35622b9927212b651481562e48cdd29aab95977
Vinny Miller <mvincent805@gmail.com>
2025-07-28 16:26:46 -0500

    Swapping matrix and point generation order.

--- python/approximating/approximate.py
+++ python/approximating/approximate.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import python.parser_helpers as ph
+import time
+import tqdm
 
 from itertools import product
 
 def enumerate_systems(args, rng):
     total_dimension = (args.dimension, args.dimension, args.dimension)
     def enumerate_from_int():
-        yield from ((-1, rng.normal(0, 1, total_dimension)) for _ in range(args.system_count))
+        yield from ((rng.normal(0, 1, total_dimension), -1) for _ in range(args.system_count))
         
     def enumerate_from_files():
         files = []
@@ -31,11 +33,17 @@ def enumerate_systems(args, rng):
     systems_generator = enumerate_from_files() if args.data_path else enumerate_from_int()
     i = 0
     for system, count in systems_generator:
-        yield approximator.SystemCache(system, count) 
+        yield approximator.SystemCache(system, count)
+        
+        # debug code to limit number of systems we approximate on
         i += 1
-        if i % 10 == 0:
+        if i % 5 == 0:
             return
         
+def generate_point_caches(args, rng):
+    dimension = args.dimension
+    return [approximator.PointCache(rng.normal(0, 1, dimension)) for _ in range(args.point_count)]
+    
 def main():
     is_positive_int = lambda value: ph.check_greater_than_int(value, 0)
     parser = argparse.ArgumentParser()
@@ -56,27 +64,31 @@ def main():
     args = parser.parse_args()
     if args.perturb >= (1 / args.dimension):
         raise ValueError('The perturbation factor >= (1 / n), {}'.format(1 / args.dimension))
+    # if args.point_count <= np.pow(args.dimension, 4):
+    #     raise ValueError('Count of points must be > n^4')
+    # if args.matrix_count <= np.pow(args.dimension, 2):
+    #     raise ValueError('Count of matrices must be > n^2')
     rng = np.random.default_rng()
     total_dimension = (args.dimension, args.dimension, args.dimension)
     coercion_values = [] if args.compare_coercion else None
     results = []
-    for system_cache in enumerate_systems(args, rng):
+    start = time.time()
+    for system_cache in tqdm.tqdm(enumerate_systems(args, rng)):
+        point_caches = generate_point_caches(args, rng)
         system_approximation = 0
-        for x in (rng.normal(0, 1, args.dimension) for _ in range(args.point_count)):
-            w_sum = 0
-            for random_system in (args.perturb * rng.normal(0, 1, total_dimension) for _ in range(args.matrix_count)):
-                random_system_cache = approximator.RandomSystemCache(system_cache, random_system, x)
+        for random_system in (args.perturb * rng.normal(0, 1, total_dimension) for _ in range(args.matrix_count)):
+            random_system_cache = approximator.RandomSystemCache(system_cache, random_system)
+            for point_cache in point_caches:
+                approimator_cache = approximator.ApproximatorCache(random_system_cache, point_cache)
                 if args.compare_coercion:
-                    coercion_values.extend(random_system_cache.get_coercion_matrix())
-                approximation, _, w = random_system_cache.get_approximation()
-                w_sum += w.prod()
+                    coercion_values.extend(approimator_cache.get_coercion_matrix())
+                approximation, _, _, _ = approimator_cache.get_approximation()
                 system_approximation += approximation
-        print(w_sum / args.matrix_count)
         system_approximation *= (1 / args.point_count) * (1 / args.matrix_count)
         results.append((system_cache.count, system_approximation))
     indices = [i for i in range(len(results))]
     counts, approximations = map(list, zip(*sorted(results)))
-    approximations = 20 * np.array(approximations)
+    print(time.time() - start)
     print(approximations)
     print(counts)
     plt.title('Approximating')
--- python/approximating/approximator.py
+++ python/approximating/approximator.py
@@ -40,88 +40,111 @@ def get_system_diagonals(system):
         system_diagonals[i, j] = system[i, j, j]
     return system_diagonals
 
-def approximate(system, system_solutions, perturbation_factor, point_count, level_set_count, rng, fail_cap):
-    """Approximates the system with the given system.
+# def approximate(system, system_solutions, perturbation_factor, point_count, level_set_count, rng, fail_cap):
+#     """Approximates the system with the given system.
 
-    Args:
-        system (N, N, N) array_like: The system of matrices.
-        system_solutions (N) array_like: The solutions to the system of matrices.
-        perturbation_factor (float64): The perturbation factor.
-        point_count (integer): The number of points to sample.
-        level_set_count (integer): The number of elements from the K_c "level set" to sample.
-        rng (RNG): The RNG to use for sampling.
+#     Args:
+#         system (N, N, N) array_like: The system of matrices.
+#         system_solutions (N) array_like: The solutions to the system of matrices.
+#         perturbation_factor (float64): The perturbation factor.
+#         point_count (integer): The number of points to sample.
+#         level_set_count (integer): The number of elements from the K_c "level set" to sample.
+#         rng (RNG): The RNG to use for sampling.
 
-    Returns:
-        float64: The approximation to the number of solutions the system possesses.
-        Is not currently scaled by 2^n.
-    """
-    n = system.shape[0]
-    sum = 0
-    for _ in tqdm.tqdm(range(point_count), total=point_count):
-        point = rng.normal(0, 1, n)
-        for _ in range(level_set_count):
-            # attempts = 0
-            projected_system_tuples = project_system(system, perturbation_factor, system_solutions, point, rng, fail_cap)
-            # while any(projected_matrix is None for _, projected_matrix in projected_system_tuples) and attempts < 10:
-            #     attempts += 1
-            #     point = rng.normal(0, 1, n)
-            #     projected_system_tuples = project_system(system, perturbation_factor, system_solutions, point, rng, fail_cap)
-            if any(projected_matrix is None for _, projected_matrix in projected_system_tuples):
-                raise ValueError('System failed mega time')
-            A = np.zeros((n, n))
-            for i in range(n):
-                projected_matrix = projected_system_tuples[i][1]
-                A[i] = projected_matrix.T @ (projected_matrix @ point)
-            sum += np.abs(np.linalg.det(A))
-    return (2 ** n) * (sum / (point_count * level_set_count)) # note that I'm not scaling by 2^n here
+#     Returns:
+#         float64: The approximation to the number of solutions the system possesses.
+#         Is not currently scaled by 2^n.
+#     """
+#     n = system.shape[0]
+#     sum = 0
+#     for _ in tqdm.tqdm(range(point_count), total=point_count):
+#         point = rng.normal(0, 1, n)
+#         for _ in range(level_set_count):
+#             # attempts = 0
+#             projected_system_tuples = project_system(system, perturbation_factor, system_solutions, point, rng, fail_cap)
+#             # while any(projected_matrix is None for _, projected_matrix in projected_system_tuples) and attempts < 10:
+#             #     attempts += 1
+#             #     point = rng.normal(0, 1, n)
+#             #     projected_system_tuples = project_system(system, perturbation_factor, system_solutions, point, rng, fail_cap)
+#             if any(projected_matrix is None for _, projected_matrix in projected_system_tuples):
+#                 raise ValueError('System failed mega time')
+#             A = np.zeros((n, n))
+#             for i in range(n):
+#                 projected_matrix = projected_system_tuples[i][1]
+#                 A[i] = projected_matrix.T @ (projected_matrix @ point)
+#             sum += np.abs(np.linalg.det(A))
+#     return (2 ** n) * (sum / (point_count * level_set_count)) # note that I'm not scaling by 2^n here
         
-def project_system(system, perturbation_factor, system_solutions, point, rng, fail_cap):
-    """Performs the K_c(point) "level set" projection.
+# def project_system(system, perturbation_factor, system_solutions, point, rng, fail_cap):
+#     """Performs the K_c(point) "level set" projection.
 
-    Args:
-        system (N, N, N) array_like: The system of matrices to project.
-        system_solutions (N) array_like: The solutions to the system of matrices.
-        point (N) array_like: The point in the K_c(point) set.
-        rng (RNG): The RNG to use.
+#     Args:
+#         system (N, N, N) array_like: The system of matrices to project.
+#         system_solutions (N) array_like: The solutions to the system of matrices.
+#         point (N) array_like: The point in the K_c(point) set.
+#         rng (RNG): The RNG to use.
 
-    Raises:
-        ValueError: The projection scheme leads to a matrix with complex elements.
+#     Raises:
+#         ValueError: The projection scheme leads to a matrix with complex elements.
 
-    Returns:
-        (N, N, N): The projected system of matrices to perform the approximation
-        calculation on.
-    """
-    n = system.shape[0]
-    point_norm = np.dot(point, point)
-    projected_system_tuples = [] 
-    for i in range(n):
-        projected_matrix_tuple = _try_project_matrix(system[i], perturbation_factor, system_solutions[i], \
-            point, point_norm, rng, fail_cap)
-        projected_system_tuples.append(projected_matrix_tuple)
-    return projected_system_tuples
+#     Returns:
+#         (N, N, N): The projected system of matrices to perform the approximation
+#         calculation on.
+#     """
+#     n = system.shape[0]
+#     point_norm = np.dot(point, point)
+#     projected_system_tuples = [] 
+#     for i in range(n):
+#         projected_matrix_tuple = _try_project_matrix(system[i], perturbation_factor, system_solutions[i], \
+#             point, point_norm, rng, fail_cap)
+#         projected_system_tuples.append(projected_matrix_tuple)
+#     return projected_system_tuples
 
-def _try_project_matrix(matrix, perturbation_factor, solution, point, point_norm, rng, fail_cap):
-    random_matrix = matrix + perturbation_factor * rng.normal(0, 1, matrix.shape)
-    for i in range(fail_cap):
-        projected_matrix = _project_matrix(random_matrix, solution, point, point_norm, rng)
-        if projected_matrix is not None:
-            return (i, projected_matrix)
-        random_matrix = matrix + perturbation_factor * rng.normal(0, 1, matrix.shape)
-    return (fail_cap, None)
+# def _try_project_matrix(matrix, perturbation_factor, solution, point, point_norm, rng, fail_cap):
+#     random_matrix = matrix + perturbation_factor * rng.normal(0, 1, matrix.shape)
+#     for i in range(fail_cap):
+#         projected_matrix = _project_matrix(random_matrix, solution, point, point_norm, rng)
+#         if projected_matrix is not None:
+#             return (i, projected_matrix)
+#         random_matrix = matrix + perturbation_factor * rng.normal(0, 1, matrix.shape)
+#     return (fail_cap, None)
 
-def _project_matrix(matrix, solution, point, point_norm, rng):
-    D = matrix @ point
-    d = np.dot(D, D) - solution
-    B = matrix.T + matrix
-    b = np.dot(point, B @ point) / point_norm
-    value = ((b ** 2) / 4) - (d / point_norm)
-    if value < 0:
-        return None
-    l = -1 * (b / 2) + rng.choice([-1, 1]) * np.sqrt(value)
-    projected_matrix = np.array(matrix)
-    projected_matrix[np.diag_indices(projected_matrix.shape[0])] += l
-    return projected_matrix
+# def _project_matrix(matrix, solution, point, point_norm, rng):
+#     D = matrix @ point
+#     d = np.dot(D, D) - solution
+#     B = matrix.T + matrix
+#     b = np.dot(point, B @ point) / point_norm
+#     value = ((b ** 2) / 4) - (d / point_norm)
+#     if value < 0:
+#         return None
+#     l = -1 * (b / 2) + rng.choice([-1, 1]) * np.sqrt(value)
+#     projected_matrix = np.array(matrix)
+#     projected_matrix[np.diag_indices(projected_matrix.shape[0])] += l
+#     return projected_matrix
 
+class PointCache:
+    def __init__(self, x):
+        self.x = x
+        self.dimension = len(self.x)
+        self.x_squared = np.power(x, 2)
+        self.special_index = self._get_special_index()
+        self.x_squared_inverse = [None for _ in range(self.dimension)]
+        
+    def _get_special_index(self):
+        sorted = np.sort(self.x_squared)
+        start_index = next((i for i in range(self.dimension) if sorted[i] >= 1 / 2), None)
+        if start_index is None:
+            return self.dimension - 1
+        entry_count = self.dimension - start_index
+        median_index = start_index + (entry_count // 2 - (1 if entry_count % 2 == 0 else 0))
+        return median_index
+    
+    def get_x_squared_inverse(self, i):
+        if self.x_squared_inverse[i] is not None:
+            return self.x_squared_inverse[i]
+        self.x_squared_inverse[i] = 1 / self.x_squared[i]
+        return self.x_squared_inverse[i]
+    
 class SystemCache:
     def __init__(self, system, count):
         self.count = count
@@ -134,70 +157,54 @@ class SystemCache:
             return self.B_psd_system_diagonals[i]
         self.B_psd_system_diagonals[i] = np.array([np.dot(B[:, i], B[:, i]) for B in self.B_scaled_system])
         return self.B_psd_system_diagonals[i]
-
+    
 class RandomSystemCache:
-    def __init__(self, system_cache, random_system, x):
+    def __init__(self, system_cache, random_system):
         self.system_cache = system_cache
-        self.A_system = self.system_cache.B_scaled_system + random_system 
+        self.A_system = self.system_cache.B_scaled_system + random_system
         self.A_psd_system_diagonals = [None for _ in range(self.system_cache.dimension)]
         
-        self.x = x
-        self.partial_results = np.array([A.T @ (A @ x) for A in self.A_system])
-        self.results = x.T @ self.partial_results.T
-        
-        self.x_squared = np.power(x, 2)
-        self.x_squared_inverse = 1 / self.x_squared
-        self.special_index = self._get_special_index()
-        self.G = [None for _ in range(self.system_cache.dimension)]
-        
-    def _get_special_index(self):
-        sorted = np.sort(self.x_squared)
-        start_index = next((i for i in range(self.system_cache.dimension) if sorted[i] >= 1 / 2), None)
-        if start_index is None:
-            return self.system_cache.dimension - 1
-        entry_count = self.system_cache.dimension - start_index
-        median_index = start_index + (entry_count // 2 - (1 if entry_count % 2 == 0 else 0))
-        return median_index
-        valid = self.x_squared >= 1 / 2
-        if all(~valid):
-            return self.x_squared.argmax()
-        valid_values = np.sort(np.extract(valid, self.x_squared))
-        median = valid_values[len(valid_values) // 2 - (1 - len(valid_values) % 2)]
-        return next(i for i in range(self.system_cache.dimension) if self.x_squared[i] == median)
-    
-    def _get_A_diagonals(self, i):
+    def get_A_diagonals(self, i):
         if self.A_psd_system_diagonals[i] is not None:
             return self.A_psd_system_diagonals[i]
         self.A_psd_system_diagonals[i] = np.array([np.dot(A[:, i], A[:, i]) for A in self.A_system])
         return self.A_psd_system_diagonals[i]
+
+class ApproximatorCache:
+    def __init__(self, random_system_cache, point_cache):
+        self.random_system_cache = random_system_cache
+        self.point_cache = point_cache
+        
+        # self.partial_results = np.array([A.T @ (A @ self.point_cache.x) for A in self.random_system_cache.A_system])
+        self.partial_results = self.random_system_cache.A_system @ self.point_cache.x
+        for i in range(len(self.partial_results)):
+            self.partial_results[i] = self.random_system_cache.A_system[i].T @ self.partial_results[i]
+        self.results = self.point_cache.x.T @ self.partial_results.T
+        self.G = [None for _ in range(self.random_system_cache.system_cache.dimension)]
     
     def get_g(self, i):
         if self.G[i] is not None:
             return self.G[i]
-        x_A_diagonals = self._get_A_diagonals(i) * self.x_squared[i]
-        self.G[i] = self.x_squared_inverse[i] * (self.system_cache.scaled_solutions - self.results + x_A_diagonals)
+        x_A_diagonals = self.random_system_cache.get_A_diagonals(i) * self.point_cache.get_x_squared_inverse(i)
+        self.G[i] = self.point_cache.get_x_squared_inverse(i) * (self.random_system_cache.system_cache.scaled_solutions - self.results + x_A_diagonals)
         return self.G[i]
     
     def get_coercion_vector(self, i):
         g = self.get_g(i)
-        g_B_diagonals_diff = np.pow(g - self.system_cache.get_B_diagonals(i), 2)
-        A_B_diagonals_diff = np.power(self._get_A_diagonals(i) - self.system_cache.get_B_diagonals(i), 2)
-        w = (-g_B_diagonals_diff + A_B_diagonals_diff) / 2
-        for i in range(self.system_cache.dimension):
-            if w[i] < -50:
-                w[i] = -50
-            elif w[i] > 20:
-                w[i] = 20
-        w = np.exp(w)
-        return self.x[i], w, g, g_B_diagonals_diff, A_B_diagonals_diff
+        g_B_diagonals_diff = np.pow(g - self.random_system_cache.system_cache.get_B_diagonals(i), 2)
+        A_B_diagonals_diff = np.power(self.random_system_cache.get_A_diagonals(i) - self.random_system_cache.system_cache.get_B_diagonals(i), 2)
+        w = np.clip((-g_B_diagonals_diff + A_B_diagonals_diff) / 2, -10, 10)
+        return self.point_cache.x[i], w, np.exp(w), g, g_B_diagonals_diff, A_B_diagonals_diff
         
     def get_coercion_matrix(self):
-        yield from (self.get_coercion_vector(i) for i in range(self.system_cache.dimension))
+        yield from (self.get_coercion_vector(i) for i in range(self.random_system_cache.system_cache.dimension))
             
     def get_approximation(self):
-        _, w, _, _, _ = self.get_coercion_vector(self.special_index)
-        log_gradient = 1 / self.x[self.special_index]
+        _, log_w, w, _, _, _ = self.get_coercion_vector(self.point_cache.special_index)
+        log_gradient = 1 / self.point_cache.x[self.point_cache.special_index]
         D_x_G = np.array(self.partial_results) 
-        D_x_G[:, self.special_index] += (self.system_cache.scaled_solutions - self.results) * log_gradient
-        D_x_G *= -2 * self.x_squared_inverse[self.special_index]
-        return np.abs(np.linalg.det(D_x_G)) * w.prod(), D_x_G, w
+        D_x_G[:, self.point_cache.special_index] += (self.random_system_cache.system_cache.scaled_solutions - self.results) * log_gradient
+        D_x_G *= -2 * self.point_cache.get_x_squared_inverse(self.point_cache.special_index)
+        weight = np.clip(np.sum(log_w), -10, 10)
+        weight = w.prod() if -10 < weight and weight < 10 else np.exp(weight)
+        return np.abs(np.linalg.det(D_x_G)) * weight, D_x_G, log_w, w
